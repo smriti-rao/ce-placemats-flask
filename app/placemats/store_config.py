9496c86e90158f437d132b34a69fb1752629c393
Adrien Guerard <adrien.guerard@gmail.com>
2018-05-26 02:00:53 -0700

    change default host to localhost

--- app/placemats/store_config.py
+++ app/placemats/store_config.py
@@ -14,7 +14,7 @@ def _get_store(resource_name) -> BaseStore:
         if 'MONGO_URL' in os.environ:
             url = os.environ['MONGO_URL']
         else:
-            url = 'mongodb://mongodb:27017/'
+            url = 'mongodb://127.0.0.1:27017/'
         return MongoStore(pymongo.MongoClient(url)['placemats'][resource_name],
                           '/{}/'.format(resource_name))
 
