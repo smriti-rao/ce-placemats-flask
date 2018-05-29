883d9e904e97d54f39bfa20b74615eeb2a634b10
Adrien Guerard <adrien.guerard@gmail.com>
2018-05-28 22:36:18 -0700

    Add pubmed search functions

--- app/placemats/ncbi_client.py
+++ app/placemats/ncbi_client.py
@@ -1,12 +1,17 @@
 import Bio.Entrez
+from Bio import Medline
 import logging
+from app.placemats.util import *
+from collections import defaultdict, namedtuple
 
 logger = logging.getLogger(__name__)
 
 API_KEY = None
 
+PER_PAGE = 100
 
-def configure_client(email='A.N.Other@example.com', api_key=None):
+
+def configure_client(email='robot.user.12234455311456@gmail.com', api_key=None):
     """
     Must be called once before calling any of the other API's
     :param email:
@@ -30,7 +35,11 @@ def call(procedure, *args, **kwargs):
     if API_KEY:
         kwargs['api_key'] = API_KEY
     handle = procedure(*args, **kwargs)
-    output = Bio.Entrez.read(handle)
+    if 'rettype' in kwargs and kwargs['rettype'] == 'medline':
+        output = Medline.parse(handle)
+        output = [x for x in output]
+    else:
+        output = Bio.Entrez.read(handle)
     handle.close()
     return output
 
@@ -61,3 +70,52 @@ def esearch(*args, **kwargs):
     :return:
     """
     return call(Bio.Entrez.esearch, *args, **kwargs)
+
+
+def pubmed_search(term, limit=PER_PAGE, skip=0):
+    retmax = min(limit, PER_PAGE)
+    return esearch(db='pubmed', sort='relevance', term=term, retstart=skip, retmax=retmax)
+
+
+def get_medline_infos(ids):
+    infos = []
+    for ids_chunk in chunks(ids, PER_PAGE):
+        infos.extend(efetch(db='pubmed', id=ids_chunk, rettype='medline', retmode='text'))
+    return infos
+
+
+def get_pmids_for_term(term, limit):
+    articles_count = 0
+    pmids = []
+    current = pubmed_search(term)
+    current_id_list = current['IdList']
+    while articles_count < limit and current_id_list:
+        pmids.extend(current_id_list)
+        articles_count += len(current_id_list)
+        current = pubmed_search(term, skip=articles_count)
+        current_id_list = current['IdList']
+    return pmids[:limit]
+
+
+AuthorInfo = namedtuple('AuthorInfo', ['pmid_to_authors', 'author_to_pmids', 'pmid_to_articles'])
+
+
+def author_info(term, limit=10_000):
+    pmids = get_pmids_for_term(term, limit)
+    pmid_to_authors = defaultdict(set)
+    author_to_pmids = defaultdict(set)
+    pmid_to_articles = {}
+    medline_infos = get_medline_infos(pmids)
+    for m_info in medline_infos:
+        if not ('FAU' in m_info and 'PMID' in m_info):
+            logger.warning('[author_info] PMID or author name not found for term: %s ; %s', term, m_info)
+            continue
+        pmid = m_info['PMID']
+        for name in m_info['FAU']:
+            author_to_pmids[name].add(pmid)
+            pmid_to_authors[pmid].add(name)
+            pmid_to_articles[pmid] = {
+                'title': m_info.get('TI'),
+                'abstract': m_info.get('AB'),
+            }
+    return AuthorInfo(pmid_to_authors, author_to_pmids, pmid_to_articles)
