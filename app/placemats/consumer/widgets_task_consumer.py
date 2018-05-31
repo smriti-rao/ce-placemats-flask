90b0c99098eff8655d3613281164dac483fd8d7d
Adrien Guerard <adrien.guerard@gmail.com>
2018-05-30 23:51:19 -0700

    .

--- app/placemats/consumer/widgets_task_consumer.py
+++ app/placemats/consumer/widgets_task_consumer.py
@@ -1,6 +1,11 @@
 from app.placemats.consumer.consumer import BaseConsumer
+from app.placemats.stores.task_queue_config import widgets_task_queue
 
 
 class WidgetsTaskConsumer(BaseConsumer):
+
+    def __init__(self) -> None:
+        super().__init__(widgets_task_queue())
+
     def consume_one(self, task_info: dict):
         pass
