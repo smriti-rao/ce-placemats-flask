9ba5417354dacf677648a7492f13d7be68475f20
Adrien Guerard <adrien.guerard@gmail.com>
2018-05-31 00:35:03 -0700

    Add consumer to supervisord

--- app/placemats/consumer/widgets_task_consumer.py
+++ app/placemats/consumer/widgets_task_consumer.py
@@ -1,5 +1,16 @@
+import os
+import logging
+
+if os.environ.get('FLASK_ENV') == 'development':
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 from app.placemats.consumer.consumer import BaseConsumer
 from app.placemats.stores.task_queue_config import widgets_task_queue
+import time
+
+logger = logging.getLogger(__name__)
 
 
 class WidgetsTaskConsumer(BaseConsumer):
@@ -9,3 +20,16 @@ class WidgetsTaskConsumer(BaseConsumer):
 
     def consume_one(self, task_info: dict):
         pass
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            WidgetsTaskConsumer().consume_forever()
+        except KeyboardInterrupt:
+            logger.info('Ctrl-C received. Exiting...')
+            break
+        except:
+            logger.error('Error while running consumer forever. Sleeping and will try again.')
+            time.sleep(10)
+            continue
