ce3b249040f895a9eb4b85ed3649abcc9d8f9d2e
Adrien Guerard <adrien.guerard@gmail.com>
2018-06-18 23:04:01 -0700

    Fix key errors

--- app/placemats/data/geo.py
+++ app/placemats/data/geo.py
@@ -15,5 +15,13 @@ def get_country_counts(strings):
         mentions = GeoText(s).country_mentions
         for country in mentions:
             country_counts[country] += 1  # bump per string; ignore freq. of mentions within one string
-    code_to_country = {k: countries.get(k) for k in country_counts}
+    code_to_country = {}
+    keys_to_pop = []
+    for k in country_counts:
+        try:
+            code_to_country[k] = countries.get(k)
+        except Exception:
+            keys_to_pop.append(k)
+    for k in keys_to_pop:
+        country_counts.pop(k, None)
     return country_counts, code_to_country
