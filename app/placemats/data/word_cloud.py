07c515c0f7ea34d6341c42c4c421acd6c5bce0bc
NageshTembhurnikar <nagesh@introp.net>
2018-07-02 19:22:58 +0530

    word cloud to show top 100 relevant terms

--- app/placemats/data/word_cloud.py
+++ app/placemats/data/word_cloud.py
@@ -2,14 +2,34 @@ from datetime import datetime
 
 
 def word_cloud(pmid_to_keywords: dict, keyword_to_pmids: dict, pmid_to_articles):
+    NUMBER_OF_KEYWORDS = 100
     word_cloud_result = []
-    for each_keyword, pmids in keyword_to_pmids.items():
-        count = len(pmids)
-        min_year_result = get_earliest_year(pmids, pmid_to_articles)
-        if min_year_result['year'] == 0:
-            continue
-        pub_time = datetime(year=int(min_year_result['year']), month=int(1), day=int(1))
-        word_cloud_result.append({ 'count': count, 'term': each_keyword, 'publication_time': pub_time.isoformat(), 'id': min_year_result['pmid'] })
+    keyword_pmid_count = {}
+
+    sorted_by_value = sorted(keyword_to_pmids.items(), key=lambda kv: len(kv[1]), reverse=True)
+
+    for index in range(NUMBER_OF_KEYWORDS):
+        each_keyword = sorted_by_value[index][0]
+        pmids = keyword_to_pmids[each_keyword]
+        year_word_cloud = {}
+        keyword_pmid_count[each_keyword] = len(pmids)
+
+        for each_pmid in pmids:
+            article_info = pmid_to_articles[each_pmid]
+
+            if article_info is None:
+                continue
+
+            article_pub_year = article_info.date_of_publication
+
+            if article_pub_year in year_word_cloud:
+                year_word_cloud[article_pub_year]['count'] = year_word_cloud[article_pub_year]['count'] + 1
+            else:
+                pub_time = datetime(year=int(article_pub_year), month=int(1), day=int(1))
+                year_word_cloud[article_pub_year] = \
+                    {'count': 1, 'term': each_keyword, 'publication_time': pub_time.isoformat(), 'id': article_pub_year}
+                word_cloud_result.append(year_word_cloud[article_pub_year])
+
     return word_cloud_result
 
 
