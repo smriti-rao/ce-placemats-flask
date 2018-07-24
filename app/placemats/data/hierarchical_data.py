e01c202317c2477526e25ade4f47365f01a97139
smriti-rao <40043594+smriti-rao@users.noreply.github.com>
2018-07-24 08:13:07 -0700

    limited # of keywords

--- app/placemats/data/hierarchical_data.py
+++ app/placemats/data/hierarchical_data.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import logging
 from itertools import combinations
+from collections import defaultdict
+from app.placemats.data.ncbi_client import get_mesh_category
 
 logger = logging.getLogger(__name__)
 
@@ -10,30 +12,35 @@ def hierarchical_data(edge_to_nodes: dict, node_whitelist: set=None):
     keywords = []
     keyword_category_dict = defaultdict()
 
-
+    key_count = 0
     for edges, nodes in edge_to_nodes.items():
         new_nodes = []
-        for n in nodes:
-            if n in keyword_category_dict:
-                new_nodes.append(keyword_category_dict[n])
-            else:
-                renamed_node = get_mesh_category(n) + '.' + n
-                renamed_node = renamed_node.replace(' and ','+')
-                renamed_node = renamed_node.replace('_and_', '+')
-                #renamed_node = renamed_node.replace(' ', '')
-                new_nodes.append(renamed_node)
-                keyword_category_dict[n] = renamed_node
-        keywords.extend(new_nodes)
-        for n1, n2 in combinations(new_nodes, 2):
-            if node_whitelist is not None and (n1 not in node_whitelist or n2 not in node_whitelist):
-                continue
-            if graph.has_edge(n1, n2):
-                graph[n1][n2]['weight'] += 1
-            else:
 
-                graph.add_edge(n1, n2, weight=1)
+        if key_count >= 135: # limit keywords to less than 150
+            break
+        else:
+            for n in nodes:
+                if n in keyword_category_dict:
+                    new_nodes.append(keyword_category_dict[n])
+                else:
+                    renamed_node = get_mesh_category(n) + '.' + n
+                    renamed_node = renamed_node.replace(' and ', '+')
+                    renamed_node = renamed_node.replace('_and_', '+')
+                    # renamed_node = renamed_node.replace(' ', '')
+                    new_nodes.append(renamed_node)
+                    keyword_category_dict[n] = renamed_node
+                    key_count += 1
 
-    print(keywords)
+            keywords.extend(new_nodes)
+
+            for n1, n2 in combinations(new_nodes, 2):
+                if node_whitelist is not None and (n1 not in node_whitelist or n2 not in node_whitelist):
+                    continue
+                if graph.has_edge(n1, n2):
+                    graph[n1][n2]['weight'] += 1
+                else:
+                    graph.add_edge(n1, n2, weight=1)
+
 
     co_occurences = []
 
